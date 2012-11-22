c60c6003d59a1d3192bb589630a29848fb1067d9
Hervé Martinet <herve.martinet@blues2.ch>
2012-11-22 15:30:33 +0100

    integrate bss_web_test_warning

---
+++ bss_web_test_warning/__openerp__.py
@@ -0,0 +1,37 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2009 Tiny SPRL (<http://tiny.be>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'Bluestar test warning',
+    'version': 'default',
+    "category" : 'Bluestar/Generic web module',
+    'complexity': "easy",
+    'description': """""",
+    'author': 'bluestar solutions sàrl',
+    'website': 'http://www.blues2.ch',
+    'depends': ['web'],
+    "js": [],
+    "css": ['static/src/css/test_warning.css'],
+    'auto_install': False,
+    'web_preload': False,
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
