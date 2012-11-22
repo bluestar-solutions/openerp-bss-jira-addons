53e599eb5d97af586ee4b6a92d66852808866ce3
Hervé Martinet <herve.martinet@blues2.ch>
2012-11-22 14:28:05 +0100

    integrate marital_status

---
+++ marital_status/marital_status.py
@@ -0,0 +1,18 @@
+# -*- encoding: utf-8 -*-
+
+from osv import osv, fields
+
+class bluestar_marital_status(osv.osv):
+
+    _name = 'bluestar.marital_status'
+    _description = "Marital status"
+
+    _columns = {
+        'name': fields.char('Nom', size=20, required=True, translate=True),
+    }
+
+    _defaults = {
+
+    }
+
+bluestar_marital_status()
