8c738d544efd3759caea2559e1f1acc5df677c2c
Hervé Martinet <herve.martinet@blues2.ch>
2012-11-22 14:27:06 +0100

    integrate resident_permit

---
+++ resident_permit/resident_permit.py
@@ -0,0 +1,19 @@
+# -*- encoding: utf-8 -*-
+
+from osv import osv, fields
+
+class bluestar_resident_permit(osv.osv):
+
+    _name = 'bluestar.resident_permit'
+    _description = "Resident permit"
+
+    _columns = {
+        'name': fields.char('Name', size=20, required=True, translate=True),
+        'description': fields.char('Description', size=200, required=True, translate=True),
+    }
+
+    _defaults = {
+
+    }
+
+bluestar_resident_permit()
