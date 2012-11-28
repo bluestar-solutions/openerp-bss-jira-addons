8cb2bfe85a8ac5ffa9dafa7bf99936ac2f5b3409
Hervé Martinet <herve.martinet@blues2.ch>
2012-11-28 08:35:03 +0100

    set version

--- bss_bank_ch/__openerp__.py
+++ bss_bank_ch/__openerp__.py
@@ -21,7 +21,7 @@
 
 {
     'name': 'Bluestar - Swiss banks',
-    'version': 'default',
+    'version': '$',
     "category" : 'Bluestar/Generic module',
     'complexity': "easy",
     'description': """A list of all CH banks.""",
