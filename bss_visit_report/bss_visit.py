5a94c7249e442e79212dab8faaafb3d523cd633c
Hervé Martinet <herve.martinet@blues2.ch>
2013-02-05 11:25:16 +0100

    Visit form

--- bss_visit_report/bss_visit.py
+++ bss_visit_report/bss_visit.py
@@ -1,8 +1,10 @@
 # -*- encoding: utf-8 -*-
 
 from openerp.osv import osv, fields
+from openerp.tools.translate import _
 
-STATE = (('draft', 'Draft'), 
+STATE = (('draft', 'Draft'),
+         ('pending', 'Pending'),
          ('terminated', 'Terminated'))
 
 class bss_visit(osv.osv):
@@ -13,7 +15,7 @@ class bss_visit(osv.osv):
 
     _columns = {
         'ref': fields.char('Reference', size=64, select=1, readonly=True),    
-        'project_id': fields.many2one('project.project', string="Project"),
+        'project_id': fields.many2one('project.project', string="Project", readonly=True, states={'draft': [('readonly', False)]}),
         'customer_id': fields.related('project_id',
                                       'partner_id',
                                       type="many2one",
@@ -28,17 +30,18 @@ class bss_visit(osv.osv):
                                           string="Project Members",
                                           store=False,
                                           readonly=True),
-        'user_id': fields.many2one('res.users', string="Visitor"),
-        'visit_task_id': fields.many2one('project.task', string="Visit Task"),
-        'initial_task_ids': fields.many2many('project.task', 'bss_visit_report_visit_initial_task_rel', 'visit_id', 'task_id', string='Initial Tasks'),
-        'done_task_ids': fields.many2many('project.task', 'bss_visit_report_visit_done_task_rel', 'visit_id', 'task_id', string='Done Tasks'),
-        'undone_task_ids': fields.many2many('project.task', 'bss_visit_report_visit_undone_task_rel', 'visit_id', 'task_id', string='Undone Tasks'),
+        'user_id': fields.many2one('res.users', string="Visitor", readonly=False, states={'terminated': [('readonly', True)]}),
+        'linked_task_id': fields.many2one('project.task', string="Visit Task", readonly=True),
+        'task_ids': fields.many2many('project.task', 'bss_visit_report_visit_initial_task_rel', 'visit_id', 'task_id', string='Initial Tasks',
+                                     readonly=False, states={'terminated': [('readonly', True)]}),
         'state': fields.selection(STATE, string="State"),
-        'date_from': fields.datetime("Date From"),
-        'date_to': fields.datetime("Date To"),
-        'time': fields.float("Time"),
-        'travel_time': fields.float("Time"),
-        'travel_zone': fields.many2one('bss_visit_report.travel_zone', string="Travel Zone", required=True),
+        'date': fields.date("Date", readonly=False, states={'terminated': [('readonly', True)]}),
+        'hour_from': fields.float("From", readonly=False, states={'terminated': [('readonly', True)]}),
+        'hour_to': fields.float("To", readonly=False, states={'terminated': [('readonly', True)]}),
+        'time': fields.float("Time", readonly=False, states={'terminated': [('readonly', True)]}),
+        'travel_time': fields.float("Travel Time", readonly=False, states={'terminated': [('readonly', True)]}),
+        'travel_zone': fields.many2one('bss_visit_report.travel_zone', string="Travel Zone",
+                                       readonly=False, states={'terminated': [('readonly', True)]}),
     }
 
     _defaults = {
@@ -50,5 +53,44 @@ class bss_visit(osv.osv):
         if not 'ref' in vals:
             vals['ref'] = self.pool.get('ir.sequence').get(cr, uid, 'bss_visit_report.visit.ref')
         return super(bss_visit, self).create(cr, uid, vals, context=context)
+    
+    def action_validate(self, cr, uid, ids, context=None):
+        for visit in self.browse(cr, uid, ids, context):
+            task_id = self.pool.get('project.task').create(cr, uid, {
+                'name': _('Visit'),
+                'description': '',
+                'project_id': visit.project_id.id,
+                'user_id': visit.user_id.id,
+                'partner_id': visit.customer_id.id,
+            }, context)
+            self.write(cr, uid, visit.id, {'state': 'pending', 'linked_task_id': task_id}, context)
+
+    def action_terminate(self, cr, uid, ids, context=None):
+        for visit in self.browse(cr, uid, ids, context):
+            if visit.hour_to - visit.hour_from <= 0.0:
+                raise osv.except_osv(_('Error'), _('Attendance difference must be greater than 00:00 !'))
+            if not visit.time:
+                raise osv.except_osv(_('Error'), _('Visit time must be greater than 00:00 !'))
+            if not visit.travel_zone:
+                raise osv.except_osv(_('Error'), _('Travel zone must be set !'))
+            self.pool.get('project.task').write(cr, uid, visit.linked_task_id.id, 
+                                                {'user_id': visit.user_id.id}, context)
+        self.write(cr, uid, ids, {'state': 'terminated'}, context)
+
+    def action_reopen(self, cr, uid, ids, context=None):
+        self.write(cr, uid, ids, {'state': 'pending'}, context)
+    
+    def onchange_project_id(self, cr, uid, ids, project_id): 
+        v={}
+        
+        if project_id:
+            project = self.pool.get('project.project').browse(cr, uid, project_id)
+            v['task_ids'] = []
+            for task in project.tasks:
+                if task.state != 'done' and task.state != 'cancelled':
+                    v['task_ids'].append(task.id)
+            v['customer_id'] = project.partner_id.id
+
+        return {'value': v} 
 
 bss_visit()
