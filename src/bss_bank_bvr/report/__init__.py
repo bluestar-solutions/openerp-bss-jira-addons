2e308f3cac4020fc3634dd41d5d979a5b3af8576
Hervé Martinet <herve.martinet@blues2.ch>
2013-06-26 14:38:52 +0200

    Fix font access

--- src/bss_bank_bvr/report/__init__.py
+++ src/bss_bank_bvr/report/__init__.py
@@ -2,7 +2,7 @@
 ##############################################################################
 #    
 #    OpenERP, Open Source Management Solution
-#    Copyright (C) 2012-2013 Bluestar Solutions Sàrl (<http://www.blues2.ch>).
+#    Copyright (C) 2013 Bluestar Solutions Sàrl (<http://www.blues2.ch>).
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
@@ -19,10 +19,7 @@
 #
 ##############################################################################
 
-# From this project
-from . import report
 
-#import wizard
-#import report
+from . import report_webkit_html
+
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
-
