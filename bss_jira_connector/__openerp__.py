2b3fd2a555a6761856437c6b4cc209591eadd95a
Hervé Martinet <herve.martinet@blues2.ch>
2013-07-16 11:51:07 +0200

    Fix module author name

--- bss_jira_connector/__openerp__.py
+++ bss_jira_connector/__openerp__.py
@@ -25,7 +25,7 @@
     "category" : 'Bluestar/Generic module',
     'complexity': "easy",
     'description': """A module to get worklogs from JIRA.""",
-    Bluestar Solutions Sàrl
+    'author': 'Bluestar Solutions Sàrl',
     'website': 'http://www.blues2.ch',
     'depends': ['base',
                 'project',
