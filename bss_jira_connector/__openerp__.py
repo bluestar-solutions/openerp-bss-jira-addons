8addd7ce86f664dbb0c5bb6fd2d0ac134c08f648
Hervé Martinet <herve.martinet@blues2.ch>
2013-09-02 15:12:16 +0200

    Set version

--- bss_jira_connector/__openerp__.py
+++ bss_jira_connector/__openerp__.py
@@ -21,7 +21,7 @@
 
 {
     'name': 'JIRA connector',
-    'version': '7.0.2.0',
+    'version': '7.0.2.1',
     "category" : 'Bluestar/Generic module',
     'complexity': "easy",
     'description': """
